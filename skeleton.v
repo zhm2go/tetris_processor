e6d44e02f82452f3cc8c6e12c42cde1d6f215abc
lxh2309 <55059295+lxh2309@users.noreply.github.com>
2019-12-05 11:33:48 +0800

    v2

--- skeleton.v
+++ skeleton.v
@@ -17,8 +17,7 @@ module skeleton(resetn,
 	count,
 	left,
 	right,
-	up,
-	down);	
+	rot);	
 	////////////////////////	VGA	////////////////////////////
 	output			VGA_CLK;   				//	VGA Clock
 	output			VGA_HS;					//	VGA H_SYNC
@@ -43,7 +42,7 @@ module skeleton(resetn,
 	
 	
 	
-	input left, right, up, down;
+	input left, right, rot;
 	
 	wire			 clock;
 	wire			 lcd_write_en;
@@ -66,7 +65,7 @@ module skeleton(resetn,
 	parameter width_score = 3;
 	parameter height_score = 5;
 	
-	//states for rotation 4 bit
+	//states
 	parameter Snew = 4'b0000;
 	parameter ScheckFall = 4'b0001;
 	parameter Sfall = 4'b0010;
@@ -74,62 +73,75 @@ module skeleton(resetn,
 	parameter Sleft = 4'b0100;
 	parameter Sright = 4'b0101;
 	parameter Sdelete = 4'b0111;
-	parameter Sidle = 4'b1111;
-	parameter Srotate = 4'b0110;
-	parameter Srotate_c = 4'b1000;
+	parameter Srot = 4'b1000;
+	parameter ScheckOver = 4'b1110;
+	parameter Sover = 4'b1111;
+	
 	
 	reg [width - 1 : 0] a[height - 1 : 0];
 	reg [4:0] x1, x2, x3, x0;
 	reg [4:0] y1, y2, y3, y0;
-	reg [31:0] terType;
+	reg [31:0] terType, terType1;
 	output count;
-	reg [3:0] curr, next;
+	reg [3:0] curr;
+	reg [3:0] game_state, next;
 	
 	reg [4:0] i;
 	reg count_neg;
 	reg [4:0] mx;
 	reg [4:0] my;
 	reg [4:0] mz;
-	reg n;
+	reg [4:0] mk;
+	reg [4:0] overi, overj;
+	reg n, m, isover;
+	reg [4:0] speedup;
 	reg [31:0] score;
 	reg [width_score - 1 : 0] num1[height_score - 1 : 0];
 	reg [width_score - 1 : 0] num2[height_score - 1 : 0];
 	
 	//for test
 	reg [7:0] ledReg;
+	reg [31:0] score_test;
 	
 	PS2_Interface myps2(clock, resetn, ps2_clock, ps2_data, ps2_key_data, ps2_key_pressed, ps2_out);
 	ps2_helper myps2Helper(count, ps2_out, myps2_out);
 //	reg [7:0] ps;
-//	ps_read(count, ps2_key_pressed, ps2_out, ps);
+//	ps_read(count, ps2_key_pressed, ps2_out, ps);	
 	
+	//behav_counter mycounter(clock, count);
+	//ClkDivider (clock, count);
+	convertNum myconvert(score, count, num1, num2);
+	
+	reg [31:0] c, d;
 	initial
 	begin
 		curr = Snew;
 		next = Snew;
-//		for(i = 0; i < height; i = i + 1) begin
-//			a[i] = 10'b0000000000;
-//		end
-//		a[0][0] = 1'b1;
-//		a[0][9] = 1'b1;
-//		a[19][9] = 1'b1;
-//		a[19][0] = 1'b1;
+		c = 10000000;
+		speedup = 1;
+		d = 10000000;
+		terType1 = 0;
 	end
+	ClkDividerNew (clock, count, d);
 	
-	//behav_counter mycounter(clock, count);
-	ClkDivider (clock, count);
-	convertNum myconvert(score, count, num1, num2);
+	always @(posedge clock) begin
+		if(score >= 10) speedup = 2;
+		else if (score >= 20) speedup = 3;
+		else speedup = 1;
+		d = c / speedup;
+	end
+		
+	always @(negedge count) begin
+		curr = next;
+	end
 	
 	always @(posedge count) 
 	begin
-//		a[0][0] = 1'b1;
-//		a[0][9] = 1'b1;
-//		a[19][9] = 1'b1;
-//		a[19][0] = 1'b1;
-		curr <= next;
 		case(curr)
 			Snew: begin
-				terType = 1;
+				score = score + 1;
+				terType1 = terType1 + 1;
+				terType = terType1 % 13 + 1;
 				case (terType)
 					1: begin
 						a[0][width / 2] = 1'b1;
@@ -163,11 +175,11 @@ module skeleton(resetn,
 						a[1][width / 2] = 1'b1;
 						a[0][width / 2 + 1] = 1'b1;
 						a[1][width / 2 + 1] = 1'b1;
-						a[0][width / 2] = 1'b1;
+						a[2][width / 2] = 1'b1;
 						x0 = 1;
 						x1 = 0;
 						x2 = 1;
-						x3 = 0;
+						x3 = 2;
 						y0 = width / 2;
 						y1 = width / 2 + 1;
 						y2 = width / 2 + 1;
@@ -314,452 +326,50 @@ module skeleton(resetn,
 						y3 = width / 2;
 					end
 				endcase
-				next <= ScheckKey;
+				next = ScheckKey;
 			end
 			
 			ScheckKey:begin
 				if(left == 0 && y0 >= 1 && y1 >= 1 && y2 >= 1 && y3 >= 1) begin
 					case(terType)
-						1: begin
-							if(a[x0][y0 - 1] == 0 && a[x2][y2 - 1] == 0) begin
-								next = Sleft;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						2: begin
-							if(a[x0][y0 - 1] == 0 && a[x2][y2 - 1] == 0)begin
-								next = Sleft;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						3: begin
-							if(a[x1][y1 - 1] == 0 && a[x0][y0 - 1] == 0 && a[x3][y3 - 1] == 0)begin
-								next = Sleft;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						4: begin
-							if(a[x0][y0 - 1] == 0)begin
-								next = Sleft;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						5: begin
-							if(a[x0][y0 - 1] == 0 && a[x1][y1 - 1] == 0 && a[x2][y2 - 1] == 0 && a[x3][y3 - 1] == 0)begin
-								next = Sleft;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						6: begin
-							if(a[x0][y0 - 1] == 0 && a[x1][y1 - 1] == 0)begin
-								next = Sleft;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						7: begin
-							if(a[x0][y0 - 1] == 0 && a[x2][y2 - 1] == 0 && a[x3][y3 - 1] == 0)begin
-								next = Sleft;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						8: begin
-							if(a[x0][y0 - 1] == 0 && a[x3][y3 - 1] == 0)begin
-								next = Sleft;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						9: begin
-							if(a[x0][y0 - 1] == 0 && a[x1][y1 - 1] == 0 && a[x2][y2 - 1] == 0)begin
-								next = Sleft;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						10: begin
-							if(a[x0][y0 - 1] == 0 && a[x3][y3 - 1] == 0)begin
-								next = Sleft;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						11: begin
-							if(a[x0][y0 - 1] == 0 && a[x1][y1 - 1] == 0 && a[x3][y3 - 1] == 0)begin
-								next = Sleft;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						12: begin
-						if(a[x0][y0 - 1] == 0 && a[x1][y1 - 1] == 0)begin
-								next = Sleft;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						13: begin
-							if(a[x0][y0 - 1] == 0 && a[x1][y1 - 1] == 0 && a[x3][y3 - 1] == 0)begin
-								next = Sleft;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-					endcase
-				end
-				else if(right == 0 && y0 < width - 2 && y1 < width - 2 && y2 < width - 2 && y3 < width - 2) begin
-					case(terType)
-						1: begin
-							if(a[x1][y1 + 1] == 0 && a[x3][y3 + 1] ==0)begin
-								next = Sright;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						2: begin
-							if(a[x1][y1 + 1] == 0 && a[x3][y3 + 1] ==0)begin
-								next = Sright;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						3: begin
-							if(a[x1][y1 + 1] == 0 && a[x2][y2 + 1] == 0 && a[x3][y3 + 1] == 0)begin
-								next = Sright;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						4: begin
-							if(a[x3][y3 + 1] ==0)begin
-								next = Sright;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						5: begin
-							if(a[x0][y0 + 1] == 0 && a[x1][y1 + 1] == 0 && a[x2][y2 + 1] == 0 && a[x3][y3 + 1] ==0)begin
-								next = Sright;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						6: begin
-							if(a[x0][y0 + 1] == 0 && a[x3][y3 + 1] ==0)begin
-								next = Sright;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						7: begin
-							if(a[x1][y1 + 1] == 0 && a[x2][y2 + 1] == 0 && a[x3][y3 + 1] ==0)begin
-								next = Sright;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						8: begin
-							if(a[x2][y2 + 1] == 0 && a[x3][y3 + 1] ==0)begin
-								next = Sright;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						9: begin
-							if(a[x1][y1 + 1] == 0 && a[x2][y2 + 1] == 0 && a[x3][y3 + 1] ==0)begin
-								next = Sright;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						10: begin
-							if(a[x2][y2 + 1] == 0 && a[x3][y3 + 1] == 0)begin
-								next = Sright;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						11: begin
-							if(a[x1][y1 + 1] == 0 && a[x2][y2 + 1] == 0 && a[x3][y3 + 1] ==0)begin
-								next = Sright;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						12: begin
-							if(a[x1][y1 + 1] == 0 && a[x3][y3 + 1] ==0)begin
-								next = Sright;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						13: begin
-							if(a[x0][y0 + 1] == 0 && a[x2][y2 + 1] == 0 && a[x3][y3 + 1] ==0)begin
-								next = Sright;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-					endcase
-				end
-				else if(up == 0)begin
-					case(terType)
-						1: begin
+					1: begin
+						if(a[x0][y0 - 1] == 0 && a[x2][y2 - 1] == 0)
+							next = Sleft;
+						else
 							next = ScheckFall;
 						end
-						2: begin
-							if(a[x0 + 1][y0] == 0 && a[x1 - 1][y1] == 0 && x0 > 0)begin
-								next = Srotate;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						3: begin
-							if(y2 <= width - 2 && a[x2 + 1][y2] == 0 && a[x2 + 1][y2 + 1] == 0)begin
-								next = Srotate;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						4: begin
-							if(y1 >= 1 && y1 <= width - 3 && a[x1][y1 - 1] == 0 && a[x1][y1 + 1] == 0 && a[x1][y1 + 2] == 0)begin
-								next = Srotate;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						5: begin
-							if(x1 >= 1 && x1 <= width - 3 && a[x1 - 1][y1] == 0 && a[x1 + 1][y1] == 0 && a[x1 + 2][y2] == 0)begin
-								next = Srotate;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						6: begin
-							if(x1 <= height - 3 && a[x1 + 1][y1] == 0 && a[x1 + 2][y1] == 0)begin
-								next = Srotate;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						7: begin
-							if(y0 >= 2 && a[x0][y0 - 1] == 0 && a[x0][y0 - 2] == 0)begin
-								next = Srotate;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						8: begin
-							if(x2 >= 2 && a[x2 - 1][y2] == 0 & a[x2 - 2][y2] == 0)begin
-								next = Srotate;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						9: begin
-							if(y3 <= width - 3 && a[x3][y3 + 1] == 0&& a[x3][y3 + 2] == 0)begin
-								next = Srotate;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						10: begin
-							if(x1 >= 1 && a[x1 - 1][y1] == 0)begin
-								next = Srotate;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						11: begin
-							if(y2 <= width - 2 && a[x2][y2 + 1] == 0)begin
-								next = Srotate;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						12: begin
-							if(x2 <= height - 2 && a[x2 + 1][y2] == 0)begin
-								next = Srotate;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
-						13: begin
-							if(y1 >= 1 && a[x1][y1 - 1] == 0)begin
-								next = Srotate;
-							end
-							else begin
-								next = ScheckFall;
-							end
-						end
 					endcase
 				end
-//				else if(down == 0)begin
-//					case(terType)
-//						1:begin
-//							next = ScheckFall;
-//						end
-//						2:begin
-//							if()begin
-//								next = Srotate_c;						
-//							end
-//							else begin
-//								next = ScheckFall;
-//							end
-//						3:begin
-//							if()begin
-//								next = Srotate_c;						
-//							end
-//							else begin
-//								next = ScheckFall;
-//							end
-//						end
-//						4:begin
-//							if()begin
-//								next = Srotate_c;						
-//							end
-//							else begin
-//								next = ScheckFall;
-//							end	
-//						end
-//						5:begin
-//							if()begin
-//								next = Srotate_c;						
-//							end
-//							else begin
-//								next = ScheckFall;
-//							end
-//						end
-//						6:begin
-//							if()begin
-//								next = Srotate_c;						
-//							end
-//							else begin
-//								next = ScheckFall;
-//							end
-//						end
-//						7:begin
-//							if()begin
-//								next = Srotate_c;						
-//							end
-//							else begin
-//								next = ScheckFall;
-//							end
-//						end
-//						8:begin
-//							if()begin
-//								next = Srotate_c;						
-//							end
-//							else begin
-//								next = ScheckFall;
-//							end
-//						end
-//						9:begin
-//							if()begin
-//								next = Srotate_c;						
-//							end
-//							else begin
-//								next = ScheckFall;
-//							end
-//						end
-//						10:begin
-//							if()begin
-//								next = Srotate_c;						
-//							end
-//							else begin
-//								next = ScheckFall;
-//							end
-//						end
-//						11:begin
-//							if()begin
-//								next = Srotate_c;						
-//							end
-//							else begin
-//								next = ScheckFall;
-//							end
-//						end
-//						12:begin
-//							if()begin
-//								next = Srotate_c;						
-//							end
-//							else begin
-//								next = ScheckFall;
-//							end
-//						end
-//						13:begin
-//							if()begin
-//								next = Srotate_c;						
-//							end
-//							else begin
-//								next = ScheckFall;
-//							end
-//						end
-//					endcase
-//				end
+				else if(right == 0 && y0 < width - 1 && y1 < width - 1 && y2 < width - 1 && y3 < width - 1) begin
+					next = Sright;
+				end
+				//else if(ps2_out == 8'h72)
+				else if(rot == 0) begin
+					case(terType)
+					1: begin
+						next = Srot;
+					end
+					2: begin
+						if(a[x2 + 1][y2] == 1) begin
+							next = ScheckFall;
+						end
+						else begin
+							next = Srot;
+						end
+					end
+					endcase
+				end
+				else begin
+					next = ScheckFall;
+				end
 			end
 			
 			ScheckFall: begin
 				case(terType)
 					1: begin
-//						if(x2 > height -2) begin
-//							next = Sdelete;
-//							score = score + 1;
-//						end
-//						else begin
-//							if(a[x2 + 1][y2] == 1 || a[x3 + 1][y3] == 1) begin 
-//								next = Sdelete;
-//								score = score + 1;
-//							end
-//							else begin
-//								next = Sfall;
-//							end
-//						end
-						if(a[x2 + 1][y2] == 1 || a[x3 + 1][y3] || x2 > height - 2 || x3 > height - 2) begin
+						if(a[x2 + 1][y2] == 1 || a[x3 + 1][y3] == 1 || x2 > height -2 || x3 > height - 2) begin
 							next = Sdelete;
-							score = score + 1;
+							//score = score + 1;
 						end
 						else begin
 							next = Sfall;
@@ -892,7 +502,7 @@ module skeleton(resetn,
 				a[x3][y3] = 1;
 				next = ScheckKey;
 			end	
-	
+//			
 			Sleft: begin
 				a[x0][y0] = 0;
 				a[x1][y1] = 0;
@@ -924,7 +534,7 @@ module skeleton(resetn,
 				a[x3][y3] = 1;
 				next = ScheckFall;
 			end
-		
+//			
 			Sdelete: begin
 				for(mx = height - 1; mx > 0; mx = mx - 1) begin
 					n = 1;
@@ -938,39 +548,43 @@ module skeleton(resetn,
 						my = my + 1;
 					end
 				end
-				next = Snew;
+				m = 1;
+				for(mk = 0; mk < width; mk = mk + 1) begin
+						m = m & a[height - 1][mk];
+				end
+				if(m == 1) next = Sdelete;
+				else next = ScheckOver;			
 			end
-			Srotate: begin
+			
+			ScheckOver: begin
+				isover = 1'b0;
+				for(overi = 0; overi < width; overi  = overi + 1) begin
+					isover = isover | a[0][overi];
+				end
+				if (isover == 1) next = Sover;
+				else next = Snew;
+			end
+			
+			Sover: begin
+				next = Sover;
+//				for(overj = 0; overj < height; overj = overj + 1) begin
+//					for(overk = 0; overk < width; overk = overk + 1) begin
+//					end	
+//				end
+			end
+					
+			Srot: begin
 				case(terType)
-					2:begin
+					1: begin
+						next = ScheckFall;
 					end
-					3:begin
-					end
-					4:begin
-					end
-					5:begin
-					end
-					6:begin
-					end
-					7:begin
-					end
-					8:begin
-					end
-					9:begin
-					end
-					10:begin
-					end
-					11:begin
-					end
-					12:begin
-					end
-					13:begin
-					end
+//					2: begin
+//						if(a[x2 + 1][y2] == 1) begin
+							
 				endcase
 			end
-			Srotate_c: begin
-			end
-		endcase
+				
+endcase
 end						
 				
 //			Sidle: begin
@@ -1011,10 +625,10 @@ end
 	lcd mylcd(clock, ~resetn, 1'b1, ps2_out, lcd_data, lcd_rw, lcd_en, lcd_rs, lcd_on, lcd_blon);
 	
 	// example for sending ps2 data to the first two seven segment displays
-	Hexadecimal_To_Seven_Segment hex1(ps2_out[3:0], seg1);
-	Hexadecimal_To_Seven_Segment hex2(ps2_out[7:4], seg2);
-	Hexadecimal_To_Seven_Segment hex3(ps2_key_pressed, seg3);
-	Hexadecimal_To_Seven_Segment hex4(ps2_key_data, seg4);
+	Hexadecimal_To_Seven_Segment hex1(a[19][3:0], seg1);
+	Hexadecimal_To_Seven_Segment hex2(a[19][7:4], seg2);
+	Hexadecimal_To_Seven_Segment hex3(a[18][3:0], seg3);
+	Hexadecimal_To_Seven_Segment hex4(a[18][7:4], seg4);
 	Hexadecimal_To_Seven_Segment hex5(myps2_out[3:0], seg5);
 	Hexadecimal_To_Seven_Segment hex6(myps2_out[7:4], seg6);
 //	Hexadecimal_To_Seven_Segment hex7(ps[3:0], seg7);
